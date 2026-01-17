12bf228e5e66ffe86e91752f3acfcd8d04659ed4
3038ICERobotics <3038robotics@isd138.org>
2026-01-16 19:58:41 -0600

    end of day commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,15 +5,32 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static class MotorIDs {
+    public static final int[] DriveIDs = new int[] { 0, 1, 2, 3 };
+    public static final int[] SteerIDs = new int[] { 4, 5, 6, 7 };
+    public static final int ShooterPrime = 8;
+    public static final int ShooterFollow = 9;
+    public static final int Transfer = 10;
+    public static final int IntakePrime = 11;
+    public static final int IntakeFollow = 12;
+    public static final int ClimbPrime = 13;
+    public static final int ClimbFollow = 14;
+
   }
 }
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,55 @@
+package frc.robot.subsystems;
+
+import java.util.ResourceBundle.Control;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ShooterSubsystem extends SubsystemBase {
+    private SparkBaseConfig config;
+    private double targetVelocity = 0;
+    private final SparkFlex shooterPrime;
+    private final SparkFlex shooterFollow;
+    private SparkClosedLoopController VelocityControl;
+
+    public ShooterSubsystem() {
+        shooterPrime = new SparkFlex(Constants.MotorIDs.ShooterPrime, null);
+        shooterFollow = new SparkFlex(Constants.MotorIDs.ShooterFollow, null);
+        config = new SparkFlexConfig();
+        config
+                .smartCurrentLimit(50)
+                .idleMode(IdleMode.kCoast)
+                .follow(shooterPrime).closedLoop.pid(0.01, 0, 0.001);
+        shooterPrime.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        shooterFollow.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        VelocityControl = shooterPrime.getClosedLoopController();
+    }
+
+    public double getCurrentSpeed() {
+        return VelocityControl.getSetpoint();
+    }
+
+    public void setMotorSpeed(double speed) {
+        VelocityControl.setSetpoint(speed, ControlType.kVelocity);
+    }
+
+    public void stopMotor() {
+        setMotorSpeed(0);
+    }
+
+    @Override
+    public void periodic() {
+        // This runs every 20ms
+        // Add any periodic updates here
+    }
+}
