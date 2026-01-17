985683b5964514ac8aff3694901b1dae2a3d619e
thetreetuna <Leifim@hotmail.com>
2026-01-17 11:53:47 -0600

    Climber started

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,5 +1,43 @@
 package frc.robot.subsystems;
 
-public class ClimberSubsystem {
-    
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ClimberSubsystem extends SubsystemBase {
+    private SparkMax climberPrime;
+    private SparkMax climberFollow;
+    private RelativeEncoder climbHeight;
+    private double retractPosition;
+    private double extendPosition;
+    private DigitalInput homeSwitch;
+
+    public ClimberSubsystem() {
+        climberPrime = new SparkMax(Constants.MotorIDs.ClimbPrime, MotorType.kBrushless);
+        climberFollow = new SparkMax(Constants.MotorIDs.ClimbFollow, MotorType.kBrushless);
+        climbHeight = climberPrime.getAlternateEncoder();
+        homeSwitch = new DigitalInput(0);
+        //TODO: Make use of constant for the channel
+    }
+
+    public double getCurrentHeight() {
+        return climbHeight.getPosition();
+    }
+
+    public void setHome() {
+        retractPosition = getCurrentHeight();
+    }
+
+    public void setSpeed(double speed) {
+        climberPrime.set(speed);
+    }
+
+    public boolean isHome() {
+        return homeSwitch.get();
+    }
+
 }
