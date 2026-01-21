531a541c8df5e906db3448038d5ba69d3dc662b0
thetreetuna <Leifim@hotmail.com>
2026-01-20 19:53:06 -0600

    Climber progessed

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,10 +1,15 @@
 package frc.robot.subsystems;
 
+import java.nio.file.DirectoryIteratorException;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -14,14 +19,26 @@ public class ClimberSubsystem extends SubsystemBase {
     private RelativeEncoder climbHeight;
     private double retractPosition;
     private double extendPosition;
-    private DigitalInput homeSwitch;
+    public Command extend;
+    public Command retract;
 
     public ClimberSubsystem() {
         climberPrime = new SparkMax(Constants.MotorIDs.ClimbPrime, MotorType.kBrushless);
         climberFollow = new SparkMax(Constants.MotorIDs.ClimbFollow, MotorType.kBrushless);
         climbHeight = climberPrime.getAlternateEncoder();
-        homeSwitch = new DigitalInput(0);
-        //TODO: Make use of constant for the channel
+        retract = new FunctionalCommand(() -> {
+            setSpeed(-.5);
+        }, () -> {
+        }, interrupted -> {
+            setSpeed(0);
+        }, this::isHome);
+        extend = new FunctionalCommand(() -> {
+            setSpeed(.5);
+        }, () -> {
+        }, interrupted -> {
+            setSpeed(0);
+        }, this::isExtended);
+        // TODO: Make use of constant for the channe
     }
 
     public double getCurrentHeight() {
@@ -37,7 +54,11 @@ public class ClimberSubsystem extends SubsystemBase {
     }
 
     public boolean isHome() {
-        return homeSwitch.get();
+        return climberPrime.getReverseLimitSwitch().isPressed();
+    }
+
+    public boolean isExtended() {
+        return climberPrime.getForwardLimitSwitch().isPressed();
     }
 
 }
