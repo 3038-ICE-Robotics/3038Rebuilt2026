d9b1d335817ad542b82eaea58f195d83883b10b4
3038ICERobotics <3038robotics@isd138.org>
2026-01-20 19:22:17 -0600

    satrted implementing drive commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,8 @@ public final class Constants {
   public static final double MetersToFeet = 3.28084;
 
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int LDriverControllerPort = 0;
+    public static final int RDriverControllerPort = 1;
   }
 
   public static class MotorIDs {
@@ -50,6 +51,8 @@ public final class Constants {
 
   public static class DriveTrain {
     public static final double WheelDiameter = (4 / 12) / MetersToFeet;
+    public static final double DrivetrainTrackWidth = (20.5/12.0)/MetersToFeet;
+    public static final double DrivetrainWheelbase = (20.5/12.0)/MetersToFeet;
     public static final double RotationsToMeters = Math.PI * WheelDiameter;
     public static final double MaxVelocityRPSEmpirical = (10 * MetersToFeet) / RotationsToMeters;
     public static final double RotationkP = 0;
@@ -59,6 +62,8 @@ public final class Constants {
     public static final String[] MotorKeys = new String[] { "FL", "FR", "BL", "BR" };
     public static final Pose2d DriveOdometryOrigin = new Pose2d(5, 5, new Rotation2d());
     public static final double MaxVelocityMPS = 5; // measure actual velocity
+    public static final double MaxAngularVelocityRadiansPS = MaxVelocityMPS
+        / Math.hypot(DrivetrainTrackWidth / 2.0, DrivetrainWheelbase / 2.0);; //measure
     public static final double DriveDeadbandMPS = 1; // measure
     public static final PIDController DRIVE_TO_POSE_X_CONTROLLER = getTranslationPIDController();
     public static final PIDController DRIVE_TO_POSE_Y_CONTROLLER = getTranslationPIDController();
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,51 +4,131 @@
 //docs.advantagekit.org/data-flow/recording-inputs/io-interfaces/
 package frc.robot;
 
+import frc.robot.Constants.DriveTrain;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.Drive;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import java.io.IOException;
+import java.util.function.DoubleSupplier;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+  private DriveSubsystem drivetrain;
+  private Drive defaultDriveCommand;
+  DoubleSupplier ControllerForwardAxisSupplier;
+  DoubleSupplier ControllerSidewaysAxisSupplier;
+  DoubleSupplier ControllerZAxisSupplier;
+  Joystick lJoystick;
+  Joystick rJoystick;
+
+  private void driveTrainInit() {
+    drivetrain = new DriveSubsystem();
+
+    defaultDriveCommand = new Drive(
+        drivetrain,
+        () -> false,
+        ControllerForwardAxisSupplier,
+        ControllerSidewaysAxisSupplier,
+        ControllerZAxisSupplier);
+    drivetrain.setDefaultCommand(defaultDriveCommand);
+  }
+
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
+    lJoystick = new Joystick(Constants.OperatorConstants.LDriverControllerPort);
+    rJoystick = new Joystick(Constants.OperatorConstants.RDriverControllerPort);
+    // Drive controls
+    ControllerSidewaysAxisSupplier = () -> modifyAxis(-lJoystick.getX(), 0);
+    ControllerForwardAxisSupplier = () -> modifyAxis(-lJoystick.getY(), 0);
+    ControllerZAxisSupplier = () -> modifyAxis(-rJoystick.getX(), 0);
     // Configure the trigger bindings
     configureBindings();
+    driveTrainInit();
+    // configureDriveTrain();
   }
 
+  private double modifyAxis(double value, double deadband) {
+    // Deadband
+    value = MathUtil.applyDeadband(value, deadband);
+    // Square the axis
+    value = Math.copySign(value * value, value);
+    return value;
+  }
+//Pathplanner TODO 
+  // private void configureDriveTrain() {
+  //   try {
+  //     AutoBuilder.configure(
+  //         drivetrain::getPose, // Pose2d supplier
+  //         drivetrain::resetOdometry, // Pose2d consumer, used to reset odometry at the beginning of auto
+  //         drivetrain::getChassisSpeeds,
+  //         (speeds) -> drivetrain.drive(speeds),
+  //         new PPHolonomicDriveController(
+  //             new com.pathplanner.lib.config.PIDConstants(
+  //                 k_XY_P, k_XY_I,
+  //                 k_XY_D), // PID constants to correct for translation error (used to create the X
+  //             // and Y PID controllers)
+  //             new com.pathplanner.lib.config.PIDConstants(
+  //                 k_THETA_P, k_THETA_I,
+  //                 k_THETA_D) // PID constants to correct for rotation error (used to create the
+  //         // rotation controller)
+  //         ),
+  //         RobotConfig.fromGUISettings(),
+  //         () -> DriverStation.getAlliance().get().equals(Alliance.Red),
+  //         drivetrain);
+  //   } catch (org.json.simple.parser.ParseException a) {
+  //     System.out.println("got ParseException trying to configure AutoBuilder");
+  //   } catch (IOException b) {
+  //     System.out.println("got IOException thrown trying to configure autobuilder " + b.getMessage());
+  //   }
+  // }
+
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
